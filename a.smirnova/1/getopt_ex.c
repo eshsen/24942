e774d1883c3c52a038691d03a8330506e424efc9
eshsen <darsyeva@mail.ru>
2025-10-22 15:56:49 +0700

    update task 1

--- a.smirnova/1/getopt_ex.c
+++ a.smirnova/1/getopt_ex.c
@@ -1,188 +1,186 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <unistd.h>
+#include <sys/resource.h>
+#include <sys/param.h>
 #include <limits.h>
 #include <string.h>
-#include <sys/resource.h>
-#include <sys/types.h>
 #include <errno.h>
+#include <ulimit.h>
 
-#define MAX_OPTIONS 50
 
-typedef struct {
-    char opt;
-    char *arg;
-} Option;
+char *strdup(const char *s) {
+    size_t len = strlen(s) + 1;
+    char *dup = malloc(len);
+    if (dup != NULL) {
+        strcpy(dup, s);
+    }
+    return dup;
+}
+
+void print_ids(){
+    printf("\n");
+    printf("Real UID: %d\n", getuid());
+    printf("Effective UID: %d\n", geteuid());
+    printf("Real GID: %d\n", getgid());
+    printf("Effective GID: %d\n", getegid());
+}
+
+void become_group_leader(){
+    printf("\n");
+    if (setpgid(0, 0) == -1){
+        perror("setpgid");
+        return;
+    }
+    printf("Process became group leader\n");
+}
+
+void print_process_ids(){
+    printf("\n");
+    printf("Process ID: %d\n", getpid());
+    printf("Parent Process ID: %d\n", getppid());
+    printf("Process Group ID: %d\n", getpgrp());
+}
+
+void print_ulimit(){
+    printf("\n");
+    printf("ulimit: %ld\n", sysconf(_SC_CHILD_MAX));
+}
+
+void change_ulimit(const char *value){
+    printf("\n");
+    if (ulimit(0, atol(value)) == -1){
+        perror("ulimit");
+        return;
+    }
+    printf("ulimit changed to %ld\n", ulimit(0));
+}
+
+void print_core_size(){
+    printf("\n");
+    struct rlimit rlim;
+    if (getrlimit(RLIMIT_CORE, &rlim) == 0){
+        printf("core size: %ld bytes\n", rlim.rlim_cur);
+    } else {
+        perror("getrlimit");
+    }
+    
+}
+
+void change_core_size(const char *value){
+    printf("\n");
+    struct rlimit rlim;
+    rlim.rlim_cur = atol(value);
+    rlim.rlim_max = rlim.rlim_cur;
+    if (setrlimit(RLIMIT_CORE, &rlim) == -1){
+        perror("setrlimit");
+        return;
+    }
+    printf("core size changed to %ld bytes\n", rlim.rlim_cur);
+}
+
+void print_current_directory(){
+    printf("\n");
+    char cwd[PATH_MAX];
+    if (getcwd(cwd, sizeof(cwd)) == NULL){
+        perror("getcwd");
+        return;
+    }
+    printf("current directory: %s\n", cwd);
+}
+
+void print_environment(){
+    printf("\n");
+    extern char **environ;
+    char **env = environ;
+
+    printf("environment variables:\n");
+    while (*env != NULL){
+        printf("%s\n", *env);
+        env++;
+    }
+}
+
+void set_environment_variable(const char *name, const char *value){
+    printf("\n");
+    if (setenv(name, value, 1) == -1){
+        perror("setenv");
+        return;
+    }
+    printf("environment variable %s set to %s\n", name, value);
+}
+
+int parse_env_name(const char *input, char **name, char **value){
+    char *equals = strchr(input, '=');
+    if (equals == NULL) {
+        perror("Invalid format for -V. Use: name=value\n");
+        return -1;
+    }
+
+    size_t name_len = equals - input;
+    *name = malloc(name_len + 1);
+    *value = strdup(equals + 1);
+
+    if (*name == NULL || *value == NULL){
+        perror("malloc/strdup");
+        free(*name);
+        *name = NULL;
+        free(*value);
+        *value = NULL;
+        return -1;
+    }
+
+    strncpy(*name, input, name_len);
+    (*name)[name_len] = '\0';
+
+    return 0;
+}
+
 
 int main(int argc, char *argv[]) {
-    int i;
-    int opt;
-    // Массив для хранения опций в порядке появления
-    Option options[MAX_OPTIONS];
-    int options_count = 0;
+    int param;
 
-    // Обработка аргументов командной строки, собираем опции
-    while ((opt = getopt(argc, argv, "isp u:: U:C:dvV:")) != -1) {
-        if (options_count >= MAX_OPTIONS) {
-            fprintf(stderr, "Превышен лимит опций\n");
-            exit(EXIT_FAILURE);
-        }
-        options[options_count].opt = opt;
-        options[options_count].arg = NULL;
-
-        // Для опций с аргументом их запоминаем
-        if (opt == 'U' || opt == 'C' || opt == 'V') {
-            options[options_count].arg = strdup(optarg);
-        }
-        options_count++;
-    }
-
-    // Обработка оставшихся аргументов после опций (если есть)
-    for (i = optind; i < argc; i++) {
-        // Можно обработать как параметры или игнорировать
-        // В данной задаче это не требуется
-    }
-
-    // Обработка опций в порядке их появления (слева направо)
-    printf("Обработка опций в порядке появления:\n");
-    for (i = 0; i < options_count; i++) {
-        char opt_char = options[i].opt;
-        switch (opt_char) {
+    while ((param = getopt(argc, argv, "ispuU:cC:dvV:")) != -1){
+        switch (param){
             case 'i':
-            {
-                printf("Real UID: %d\n", getuid());
-                printf("Effective UID: %d\n", geteuid());
-                printf("Real GID: %d\n", getgid());
-                printf("Effective GID: %d\n", getegid());
+                print_ids();
                 break;
-            }
             case 's':
-            {
-                pid_t pid = getpid();
-                pid_t pgrp = getpgrp();
-                printf("Process PID: %d, PGID: %d\n", pid, pgrp);
-                // меняем группу процесса на текущий
-                if (setpgid(0,0) == -1) {
-                    perror("setpgid");
-                } else {
-                    printf("Группа процесса изменена.\n");
-                }
+                become_group_leader();
                 break;
-            }
             case 'p':
-            {
-                printf("PID: %d, PPID: %d, PGID: %d\n", getpid(), getppid(), getpgid(0));
+                print_process_ids();
                 break;
-            }
             case 'u':
-            {
-                struct rlimit lim;
-                if (getrlimit(RLIMIT_CORE, &lim) == -1) {
-                    perror("getrlimit");
-                } else {
-                    printf("Core dump size (по умолчанию): %ld bytes\n", lim.rlim_cur);
-                }
+                print_ulimit();
                 break;
-            }
             case 'U':
-            {
-                // Изменяет ulimit (RLIMIT_CORE)
-                if (options[i].arg) {
-                    long new_lim = strtol(options[i].arg, NULL, 10);
-                    struct rlimit lim;
-                    if (getrlimit(RLIMIT_CORE, &lim) == -1) {
-                        perror("getrlimit");
-                        break;
-                    }
-                    lim.rlim_cur = new_lim;
-                    if (setrlimit(RLIMIT_CORE, &lim) == -1) {
-                        perror("setrlimit");
-                    } else {
-                        printf("uLimit установлен в %ld\n", new_lim);
-                    }
-                }
+                change_ulimit(optarg);
                 break;
-            }
             case 'c':
-            {
-                // Размер core файла
-                struct rlimit lim;
-                if (getrlimit(RLIMIT_CORE, &lim) == -1) {
-                    perror("getrlimit");
-                } else {
-                    printf("Max core file size: %ld bytes\n", lim.rlim_cur);
-                }
+                print_core_size();
                 break;
-            }
             case 'C':
-            {
-                // Установка размера core-файла
-                if (options[i].arg) {
-                    long new_size = strtol(options[i].arg, NULL, 10);
-                    struct rlimit lim;
-                    if (getrlimit(RLIMIT_CORE, &lim) == -1) {
-                        perror("getrlimit");
-                        break;
-                    }
-                    lim.rlim_cur = new_size;
-                    if (setrlimit(RLIMIT_CORE, &lim) == -1) {
-                        perror("setrlimit");
-                    } else {
-                        printf("Размер core-файла установлен в %ld\n", new_size);
-                    }
-                }
+                change_core_size(optarg);
                 break;
-            }
             case 'd':
-            {
-                char cwd[PATH_MAX];
-                if (getcwd(cwd, sizeof(cwd)) == NULL) {
-                    perror("getcwd");
-                } else {
-                    printf("Текущая директория: %s\n", cwd);
-                }
+                print_current_directory();
                 break;
-            }
             case 'v':
-            {
-                // Выводит все переменные среды
-                extern char **environ;
-                for (char **env = environ; *env != NULL; env++) {
-                    printf("%s\n", *env);
-                }
+                print_environment();
                 break;
-            }
-            case 'V':
-            {
-                // Установка переменной среды
-                if (options[i].arg) {
-                    char *eq_pos = strchr(options[i].arg, '=');
-                    if (eq_pos) {
-                        size_t name_len = eq_pos - options[i].arg;
-                        char var_name[name_len + 1];
-                        strncpy(var_name, options[i].arg, name_len);
-                        var_name[name_len] = '\0';
-                        char *value = eq_pos + 1;
-                        if (setenv(var_name, value, 1) == -1) {
-                            perror("setenv");
-                        } else {
-                            printf("Добавлено/обновлено %s=%s\n", var_name, value);
-                        }
-                    } else {
-                        fprintf(stderr, "Некорректный формат -V: %s\n", options[i].arg);
-                    }
+            case 'V': {
+                char *name, *value;
+                if (parse_env_name(optarg, &name, &value) == 0) {
+                    set_environment_variable(name, value);
+                    free(name);
+                    free(value);
                 }
                 break;
             }
             default:
-                fprintf(stderr, "Неизвестная опция -%c\n", opt_char);
+                exit(EXIT_FAILURE);
         }
-    }
-
-    // Освобождение памяти
-    for (i = 0; i < options_count; i++) {
-        if (options[i].arg) free(options[i].arg);
-    }
-
-    return 0;
+    }    
+    return EXIT_SUCCESS; 
 }
