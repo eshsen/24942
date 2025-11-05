d2134b68bb2b2ac783a5f734904de81b7873182f
eshsen <darsyeva@mail.ru>
2025-11-05 15:13:13 +0700

    add task9

---
+++ a.smirnova/9/task9.c
@@ -0,0 +1,29 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+
+int main() {
+    pid_t pid = fork(); // здаем новый процесс
+
+    if (pid == -1) {
+        perror("fork failed");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        // код выполняется в дочернем процессе
+        printf("Дочерний процесс: запуск cat");
+        execlp("cat", "cat", "/etc/passwd", NULL);
+        // если execlp вернул управление, значит произошла ошибка
+        perror("execlp failed");
+        exit(1);
+    } else {
+        //
+        printf("Родительский процесс: мой PID = %d, PID дочернего = %d\n",
+                getpid(), pid);
+        printf("Родительский процесс: печать текста\n");
+        printf("Родительский процесс: работа завершена\n");
+    }
+    return 0;
+}
