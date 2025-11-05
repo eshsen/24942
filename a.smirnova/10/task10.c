621a8ecc7be2a50795253b23ea8bc9f4c1b1b0ba
eshsen <darsyeva@mail.ru>
2025-11-05 15:55:20 +0700

    add task10

---
+++ a.smirnova/10/task10.c
@@ -0,0 +1,60 @@
+#include <stdio.h>
+#include <unistd.h>
+#include <sys/wait.h>
+#include <stdlib.h>
+#include <string.h>
+
+int main(int argc, char*argv[]) {
+    if (argc < 2) {
+        fprintf(stderr, "Использование: %s <команда> [аргументы...]\n", argv[0]);
+        exit(1);
+    }
+
+    printf("Родительский процесс: PID = %d\n", getpid());
+    printf("Запускаю команду: %s", argv[1]);
+
+    // выводим все аргументы для команды
+    for (int i = 2; i < argc; i++) {
+        printf(" %s", argv[i]);
+    }
+    printf("\n");
+
+    pid_t pid = fork();
+
+    if (pid == -1) {
+        perror("fork failed");
+        exit(1);
+    }
+
+    if (pid == 0) {
+        printf("Дочерний процесс: PID = %d\n", getpid());
+        char **cmd_args = malloc((argc) * sizeof(char*));
+        cmd_args[0] = argv[1]; // первый аргумент - имя команды
+        // копируем остальные аргументы
+        for (int i = 2; i < argc; i++) {
+            cmd_args[i-1] = argv[i];
+        }
+        cmd_args[argc-1] = NULL; // последний аргумент NULL
+        execvp(argv[1], cmd_args); // заменяем программу дочернего процесса на указанную команду 
+        perror("execvp failed");
+        free(cmd_args);
+        exit(1);
+    } else {
+        // ждем завершения дочернего процесса
+        printf("Родительский процесс: жду завершения дочернего процесса\n");
+        int status;
+        waitpid(pid, &status, 0);
+        if (WIFEXITED(status)) {
+            // процесс завершился нормально
+            int exit_code = WEXITSTATUS(status);
+            printf("Родительский процесс: дочерний процесс завершился с кодом = %d\n", exit_code);
+        } else if (WIFSIGNALED(status)) {
+            // процесс был завершен сигналом
+            int signal_num = WTERMSIG(status);
+            printf("Родительский процесс: дочерний процесс убит сигналом = %d\n", signal_num);
+        } else {
+            printf("Родительский процесс: дочерний процесс завершился ненормально\n");
+        }
+    }
+    return 0;
+}
