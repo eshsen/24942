ba0a6e77e22738cb1be4924f81359b7c5b73f215
eshsen <darsyeva@mail.ru>
2025-11-26 17:22:08 +0700

    add task30

---
+++ a.smirnova/30/client.c
@@ -0,0 +1,46 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+
+#define SOCKET_PATH "/tmp/task30_socket"
+#define BUFFER_SIZE 1024
+
+int main() {
+    int sockfd;
+    struct sockaddr_un server_addr;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_written;
+    
+    sockfd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (sockfd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+    
+    if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("connect");
+        close(sockfd);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Подключено к серверу. Введите текст (Ctrl+D для завершения):\n");
+    
+    while (fgets(buffer, BUFFER_SIZE, stdin) != NULL) {
+        bytes_written = write(sockfd, buffer, strlen(buffer));
+        if (bytes_written == -1) {
+            perror("write");
+            break;
+        }
+    }
+    
+    printf("Соединение закрыто\n");
+    close(sockfd);
+    return 0;
+}
---
+++ a.smirnova/30/server.c
@@ -0,0 +1,81 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <sys/socket.h>
+#include <sys/un.h>
+#include <ctype.h>
+
+#define SOCKET_PATH "/tmp/task30_socket"
+#define BUFFER_SIZE 1024
+
+int main() {
+    int server_fd, client_fd;
+    struct sockaddr_un server_addr, client_addr;
+    socklen_t client_len;
+    char buffer[BUFFER_SIZE];
+    ssize_t bytes_read;
+    
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd == -1) {
+        perror("socket");
+        exit(EXIT_FAILURE);
+    }
+    
+    unlink(SOCKET_PATH);
+    
+    memset(&server_addr, 0, sizeof(server_addr));
+    server_addr.sun_family = AF_UNIX;
+    strncpy(server_addr.sun_path, SOCKET_PATH, sizeof(server_addr.sun_path) - 1);
+    
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) == -1) {
+        perror("bind");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    
+    if (listen(server_fd, 5) == -1) {
+        perror("listen");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Сервер запущен и слушает на %s\n", SOCKET_PATH);
+    
+    client_len = sizeof(client_addr);
+    client_fd = accept(server_fd, (struct sockaddr*)&client_addr, &client_len);
+    if (client_fd == -1) {
+        perror("accept");
+        close(server_fd);
+        exit(EXIT_FAILURE);
+    }
+    
+    printf("Клиент подключен\n");
+    
+    while ((bytes_read = read(client_fd, buffer, BUFFER_SIZE - 1)) > 0) {
+        buffer[bytes_read] = '\0';
+        
+        for (int i = 0; i < bytes_read; i++) {
+            buffer[i] = toupper(buffer[i]);
+        }
+        
+        printf("Преобразованный текст: %s", buffer);
+        
+        if (strchr(buffer, '\n')) {
+            fflush(stdout);
+        }
+    }
+    
+    if (bytes_read == -1) {
+        perror("read");
+    }
+    
+    printf("Клиент отключился\n");
+    
+    close(client_fd);
+    close(server_fd);
+    
+    unlink(SOCKET_PATH);
+    
+    return 0;
+}
